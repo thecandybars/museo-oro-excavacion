c65aa753b2f8305b6935d824a11c8bab277c0790
Andrés Vilá <vilaandres@gmail.com>
2025-06-03 10:54:04 -0500

    Eliminé seccion Casa

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import { BrowserRouter, Route, Routes } from "react-router";
 import "./App.css";
 import Mapa from "./pages/mapa/MapaIndex";
-import Casa from "./pages/casa/Casa";
 import Excavacion from "./pages/excavacion/Excavacion";
 import Piezas from "./pages/piezas/Piezas";
 import Layout from "./layout/Layout";
@@ -14,7 +13,6 @@ function App() {
         <Route path="/" element={<Layout />}>
           <Route index element={<Inicio />} />
           <Route path="mapa" element={<Mapa />} />
-          <Route path="casa" element={<Casa />} />
           <Route path="excavacion" element={<Excavacion />} />
           <Route path="piezas" element={<Piezas />} />
           <Route path="*" element={<Inicio />} />
